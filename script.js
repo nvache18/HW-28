a706652e7b39c97bd219d976925b244f5ace735a
nvache18 <nikoloz.vacheishvili18@gmail.com>
2025-05-18 13:01:00 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const formSteps = document.querySelectorAll(".form-step");
+const nextBtns = document.querySelectorAll(".next-btn");
+const prevBtns = document.querySelectorAll(".prev-btn");
+const progressSteps = document.querySelectorAll(".step");
+
+let currentStep = 0;
+
+function updateStep() {
+  formSteps.forEach((step, index) => {
+    step.classList.toggle("active", index === currentStep);
+  });
+
+  progressSteps.forEach((circle, index) => {
+    circle.classList.toggle("active", index <= currentStep);
+  });
+}
+
+nextBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    const inputs = formSteps[currentStep].querySelectorAll("input");
+    for (let input of inputs) {
+      if (!input.checkValidity()) {
+        input.reportValidity();
+        return;
+      }
+    }
+    currentStep++;
+    updateStep();
+  });
+});
+
+prevBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    currentStep--;
+    updateStep();
+  });
+});
+
+document.getElementById("multiStepForm").addEventListener("submit", e => {
+  e.preventDefault();
+  alert("Form submitted successfully!");
+});
